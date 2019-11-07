eedab8c873d175f26c0b36f13fe247a1682675e0
kazankovMarch <kazankov.march@gmail.com>
2019-11-07 21:10:43 +0300

    update main frame according to tech task

--- src/main/kotlin/ru/adkazankov/scienceconference/control/MainFrameController.kt
+++ src/main/kotlin/ru/adkazankov/scienceconference/control/MainFrameController.kt
@@ -1,20 +1,62 @@
 package ru.adkazankov.scienceconference.control
 
+import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.control.Tab
 import org.springframework.stereotype.Controller
+import javafx.scene.layout.VBox
+
+
 
 @Controller
 class MainFrameController {
     @FXML
     private lateinit var presentationTab: Tab
-
     @FXML
     private lateinit var personTab: Tab
-
+    @FXML
+    private lateinit var speakerTab: Tab
     @FXML
     private lateinit var auditoryTab: Tab
-
+    @FXML
+    private lateinit var ticketTab: Tab
     @FXML
     private lateinit var queryTab: Tab
+    @FXML
+    private lateinit var showColumnVBox: VBox
+
+    @FXML
+    fun onAddAction(event: ActionEvent) {
+
+    }
+
+    @FXML
+    fun onDeleteAction(event: ActionEvent) {
+
+    }
+
+    @FXML
+    fun onEditAction(event: ActionEvent) {
+
+    }
+
+    @FXML
+    fun onFilterAction(event: ActionEvent) {
+
+    }
+
+    @FXML
+    fun onLoadAction(event: ActionEvent) {
+
+    }
+
+    @FXML
+    fun onRefreshAction(event: ActionEvent) {
+
+    }
+
+    @FXML
+    fun onSaveAction(event: ActionEvent) {
+
+    }
 }
