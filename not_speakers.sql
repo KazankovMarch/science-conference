4ab0b12ee6a3da7dcd25091ffe5dd761f3a0f44b
kazankovMarch <kazankov.march@gmail.com>
2019-11-11 22:38:21 +0300

    restart sequences

--- not_speakers.sql
+++ not_speakers.sql
@@ -1,2 +1,3 @@
-SELECT * FROM person p 
+-- участники конференции, не выступающие ни с одной презентацией
+SELECT * FROM person p
 WHERE (NOT EXISTS (SELECT * FROM  speaker s WHERE s.person_id = p.id))
